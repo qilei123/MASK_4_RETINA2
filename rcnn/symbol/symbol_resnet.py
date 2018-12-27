c672b2ab15d2c4fbf2fe1cb19ba93898ed196919
qilei123 <qileimail123@gmail.com>
2018-12-27 00:37:38 -0500

    fda

--- rcnn/symbol/symbol_resnet.py
+++ rcnn/symbol/symbol_resnet.py
@@ -41,17 +41,17 @@ def residual_unit_dcn_v1(data, num_filter, stride, dim_match, name):
                                no_bias=True, workspace=workspace, name=name + '_conv1')
     bn2 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, eps=eps, use_global_stats=use_global_stats, name=name + '_bn2')
     act2 = mx.sym.Activation(data=bn2, act_type='relu', name=name + '_relu2')
-    
+    '''
     conv2 = mx.sym.Convolution(data=act2, num_filter=int(num_filter * 0.25), kernel=(3, 3), stride=stride, pad=(1, 1),
                                no_bias=True, workspace=workspace, name=name + '_conv2')
     '''
     conv2_offset = mx.symbol.Convolution(name=name + '_conv2_offset', data = act2,
-                                                    num_filter=18, pad=(1, 1), kernel=(3, 3), stride=(1, 1), dilate=(1, 1), cudnn_off=True)
+                                                    num_filter=72, pad=(1, 1), kernel=(3, 3), stride=stride, dilate=(2, 2), cudnn_off=True)
     conv2 = mx.contrib.symbol.DeformableConvolution(name=name + '_conv2', data=act2, offset=conv2_offset,
                                                                 num_filter=int(num_filter * 0.25), pad=(1, 1), kernel=(3, 3),
                                                                 num_deformable_group=4,
-                                                                stride=stride, dilate=(1, 1), no_bias=True)    
-    ''' 
+                                                                stride=stride, dilate=(2, 2), no_bias=True)    
+     
     bn3 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, eps=eps, use_global_stats=use_global_stats, name=name + '_bn3')
     act3 = mx.sym.Activation(data=bn3, act_type='relu', name=name + '_relu3')
     conv3 = mx.sym.Convolution(data=act3, num_filter=num_filter, kernel=(1, 1), stride=(1, 1), pad=(0, 0), no_bias=True,
