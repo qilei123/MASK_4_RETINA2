e2582c8832b521498a71f5979a2b356a62e268d3
qilei123 <qileimail123@gmail.com>
2019-01-04 16:31:26 -0500

    dfa

---
+++ test_rop_dcn_v1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export EXP_ID=3
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/qileimail123/opencv32/install/lib:/usr/local/cuda/lib64
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
+export PYTHONUNBUFFERED=1
+export MXNET_ENABLE_GPU_P2P=0
+export PYTHONPATH=${PYTHONPATH}:incubator-mxnet/python/
+#python maskrcnn_train_end2end.py --gpus 2 --resume --begin_epoch 20 --end_epoch 30
+python maskrcnn_test.py --gpu 3 --epoch 20
